a4a7c6cf1c2a9f8509014f61cd3ee1619a757814
Jhon <jhon.inalien@gmail.com>
2020-09-06 14:25:44 -0400

    docs

---
+++ docs/precache-manifest.214d26e1e03fe876ac8c293df5573d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5ec77738eabac4977ad9c1055fe915",
+    "url": "/People-Finder/index.html"
+  },
+  {
+    "revision": "252ee8741dd41adb07e9",
+    "url": "/People-Finder/static/css/main.11ecdcf2.chunk.css"
+  },
+  {
+    "revision": "12010ccd7075d9a65c69",
+    "url": "/People-Finder/static/js/2.568cb915.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/People-Finder/static/js/2.568cb915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252ee8741dd41adb07e9",
+    "url": "/People-Finder/static/js/main.0ff9b942.chunk.js"
+  },
+  {
+    "revision": "0eba28dca3e38654b6c8",
+    "url": "/People-Finder/static/js/runtime-main.d391d5d4.js"
+  }
+]);
